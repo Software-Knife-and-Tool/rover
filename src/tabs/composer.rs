ed64a8b313c2cd5247f2db010a53c166724ada6e
putnamjm@closure.local <putnamjm.design@gmail.com>
2024-04-05 21:05:20 -0400

    #8 - composer: improve ui

--- src/tabs/composer.rs
+++ src/tabs/composer.rs
@@ -1,34 +1,49 @@
 //  SPDX-FileCopyrightText: Copyright 2024 James M. Putnam (putnamjm.design@gmail.com)
 //  SPDX-License-Identifier: MIT
-
+#[allow(unused_imports)]
 use {
     crate::{Message, Tab},
-    iced::{
-        widget::{Container, Text},
-        Element, Length,
+    iced::widget::{
+        button, checkbox, column, container, horizontal_space, image, row, rule, scrollable, text,
+        text_input, toggler, vertical_space, Button, Column, Container, Row, Rule, Text,
     },
-    iced_aw::{split, tab_bar::TabLabel, Split},
+    iced::{Element, Length, Renderer, Theme},
+    iced_aw::{
+        menu::{self, Item, Menu, StyleSheet},
+        split,
+        style::MenuBarStyle,
+        Split, TabLabel,
+    },
+    std::fmt::Display,
 };
 
 #[derive(Debug, Clone)]
 pub enum ComposerMessage {
-    OnHorResize(u16),
+    OnResize(u16),
+    EvalPressed,
+    ClearPressed,
 }
 
 pub struct ComposerTab {
-    hor_divider_position: Option<u16>,
+    divider_position: Option<u16>,
 }
 
 impl ComposerTab {
     pub fn new() -> Self {
         ComposerTab {
-            hor_divider_position: None,
+            divider_position: None,
         }
+    }
+
+    fn padded_button<Message: Clone>(label: &str) -> Button<'_, Message> {
+        button(text(label)).padding([12, 24])
     }
 
     pub fn update(&mut self, message: ComposerMessage) {
         match message {
-            ComposerMessage::OnHorResize(position) => self.hor_divider_position = Some(position),
+            ComposerMessage::OnResize(position) => self.divider_position = Some(position),
+            ComposerMessage::EvalPressed => (),
+            ComposerMessage::ClearPressed => (),
         }
     }
 }
@@ -45,25 +60,36 @@ impl Tab for ComposerTab {
     }
 
     fn content(&self) -> Element<'_, Self::Message> {
-        let top = Container::new(Text::new("Top"))
+        let controls = row![]
+            .push(Self::padded_button("eval").on_press(ComposerMessage::EvalPressed))
+            .push(horizontal_space())
+            .push(Self::padded_button("clear").on_press(ComposerMessage::ClearPressed));
+
+        let compose = Container::new(Text::new("compose"))
             .width(Length::Fill)
             .height(Length::Fill)
             .center_x()
             .center_y();
 
-        let bottom = Container::new(Text::new("Bottom"))
+        let eval = Container::new(Text::new("eval"))
             .width(Length::Fill)
             .height(Length::Fill)
             .center_x()
             .center_y();
 
-        let content: Element<'_, ComposerMessage> = Container::new(Split::new(
-            top,
-            bottom,
-            self.hor_divider_position,
-            split::Axis::Horizontal,
-            ComposerMessage::OnHorResize,
-        ))
+        let content: Element<'_, ComposerMessage> = Container::new(
+            Column::new()
+                .push(controls)
+                .push(Text::new(""))
+                .push(Rule::horizontal(2))
+                .push(Split::new(
+                    compose,
+                    eval,
+                    self.divider_position,
+                    split::Axis::Horizontal,
+                    ComposerMessage::OnResize,
+                )),
+        )
         .into();
 
         content.map(Message::Composer)
